b125daf6e90c8d5bfe1e48aa0a523bf1aa36503e
Nick Myers <nicholasmyers@users.noreply.github.com>
2016-03-19 18:45:37 -0700

    Revised color scheme and added new icon.

--- src/colours.h
+++ src/colours.h
@@ -2,18 +2,18 @@
 #include <pebble.h>
   
 #ifdef PBL_COLOR
-  #define COLOUR_HEADER GColorRed
+  #define COLOUR_HEADER GColorBlue
   #define COLOUR_HEADER_TEXT GColorWhite
-  #define COLOUR_WINDOW GColorWhite
-  #define COLOUR_MENU_BACKGROUND GColorWhite
-  #define COLOUR_MENU_FOREGROUND GColorBlack
-  #define COLOUR_MENU_HIGHLIGHT_BACKGROUND GColorFromHEX(0xE31837)
-  #define COLOUR_MENU_HIGHLIGHT_FOREGROUND GColorWhite
+  #define COLOUR_WINDOW GColorLightGray
+  #define COLOUR_MENU_BACKGROUND GColorLightGray
+  #define COLOUR_MENU_FOREGROUND GColorBlue
+  #define COLOUR_MENU_HIGHLIGHT_BACKGROUND GColorBlue
+  #define COLOUR_MENU_HIGHLIGHT_FOREGROUND GColorFromHEX(0xFFFF00)
   
   #define COLOUR_BULLET GColorFromHEX(0xE31837)
   #define COLOUR_LIMITED GColorChromeYellow
   #define COLOUR_LOCAL GColorDarkGray
-  #define COLOUR_SHUTTLE GColorWhite
+  #define COLOUR_SHUTTLE GColorLightGray
   
   #define COLOUR_ROUTE_OUTLINE GColorClear
   #define COLOUR_ROUTE_FILLED GColorDarkGray
